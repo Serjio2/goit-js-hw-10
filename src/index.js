10f299f882b337b2c55dc5d008f78b6ab04bf2a2
Serhii Kondratenko <kondratenko.serhii3@gmail.com>
2023-07-02 20:32:30 +0300

    update hw-10

--- src/index.js
+++ src/index.js
@@ -1,6 +1,94 @@
+import { fetchBreeds, BASE_URL, API_KEY } from './js/cat-api';
 
-import fetchBreeds from './js/cat-api';
+import SlimSelect from 'slim-select';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 
+const selectItemEl = document.querySelector('.breed-select');
+const showItemEl = document.querySelector('.cat-info');
+// const loaderTxt = document.querySelector('.loader');
+// const errorTxt = document.querySelector('.error');
+
+Loading.standard('Loading data, please wait...')
+
+selectItemEl.hidden = true;
+// loaderTxt.hidden = false;
 
 fetchBreeds()
+  .then(data => {
+    
+    Loading.remove();
+
+    selectItemEl.hidden = false;
+    // loaderTxt.hidden = false;
+
+    data.map(({ id, name }) => {
+
+      const itemList = document.createElement('option');
+      selectItemEl.append(itemList);
+
+      itemList.value = id;
+      itemList.textContent = name;
+    });
+
+    // selectItemEl.hidden = false;
+    // loaderTxt.hidden = true;
+
+    return data;
+  })
+  .catch(() => {
+    Loading.remove();
+    Notify.failure('Oops! Something went wrong! Try reloading the page!');
+    // loaderTxt.hidden = true;
+    // errorTxt.hidden = false;
+  });
+
+
+selectItemEl.addEventListener('change', fetchCatByBreed);
+
+
+function fetchCatByBreed(breedId) {
+
+  Loading.standard('Loading data, please wait...')
+  // loaderTxt.hidden = false;
+  showItemEl.innerHTML = '';
+
+  breedId = breedId.target.value;
+
+  return fetch(
+    `${BASE_URL}images/search?breed_ids=${breedId}&api_key=${API_KEY}`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      // console.log(response)
+      return response.json();
+    })
+
+    .then(data => {
+      console.log(data);
+      for (const item of data) {
+        item.breeds.map(({ name, description, temperament }) => {
+          showItemEl.innerHTML = `<ul style=list-style:none>
+                <li>
+                    <img src="${item.url}" height=400px></img>
+                    <h2>${name}</h2>
+                    <p>${description}</p>
+                    <p><b>Temperament:</b> ${temperament}</p>
+                </li>
+            </ul>`;
+        });
+      }
+      Loading.remove();
+      // loaderTxt.hidden = true;
+    })
+
+    .catch(() => {
+      Loading.remove();
+      Notify.failure('Oops! Something went wrong! Try reloading the page!');
+      // loaderTxt.hidden = true;
+      // errorTxt.hidden = false;
+    });
+}
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,83 +1,22 @@
-const selectItemEl = document.querySelector('.breed-select');
-const showItemEl = document.querySelector('.cat-info');
-const loaderTxt = document.querySelector('.loader');
-const errorTxt = document.querySelector('.error');
 
 const BASE_URL = 'https://api.thecatapi.com/v1/';
 const API_KEY =
   'live_Ltm2GBWPpfVNBkV01FC69DA1X4Xk5kGZBGw4eapAO9DTZDepX49nl7yBbmf24740';
 
-  
+
 function fetchBreeds() {
-  return fetch(`${BASE_URL}breeds?api_key=${API_KEY}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response.json();
-  });
-}
-
-selectItemEl.hidden = true;
-loaderTxt.hidden = false;
-
-fetchBreeds()
-  .then(data => {
-    selectItemEl.hidden = true;
-    loaderTxt.hidden = false;
-
-    for (const item of data) {
-      const itemList = document.createElement('option');
-      selectItemEl.append(itemList);
-      itemList.value = item.id;
-      itemList.textContent = item.name;
-    }
-
-    selectItemEl.hidden = false;
-    loaderTxt.hidden = true;
-  })
-  .catch(() => {
-    loaderTxt.hidden = true;
-    errorTxt.hidden = false;
-    // console.log("erhgfhgror")
-  });
-
-
-selectItemEl.addEventListener('change', fetchCatByBreed);
-
-
-function fetchCatByBreed(breedId) {
-  loaderTxt.hidden = false;
-  showItemEl.innerHTML = '';
-
-  fetch(
-    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId.target.value}&api_key=${API_KEY}`
-  )
+    return fetch(`${BASE_URL}breeds?api_key=${API_KEY}`)
     .then(response => {
+        
       if (!response.ok) {
-        throw Error(response.statusText);
+        throw new Error(response.statusText);
       }
-      return response.json();
-    })
-    .then(data => {
-      // console.log(name)
-
-      for (const item of data) {
-        item.breeds.map(({ name, description, temperament }) => {
-          showItemEl.innerHTML = `<ul style=list-style:none>
-                <li>
-                    <img src="${item.url}" height=400px></img>
-                    <h3>${name}</h3>
-                    <p>${description}</p>
-                    <p><b>Temperament:</b> ${temperament}</p>
-                </li>
-            </ul>`;
-        });
-      }
-      loaderTxt.hidden = true;
-    })
-
-    .catch(() => {
-      loaderTxt.hidden = true;
-      errorTxt.hidden = false;
+       return response.json();
     });
-}
+  }
+
+  
+
+
+
+  export { fetchBreeds, BASE_URL, API_KEY };
