36348d9c86cf59124e424a0084f9395328387057
Serhii Kondratenko <kondratenko.serhii3@gmail.com>
2023-07-02 23:33:52 +0300

    finshed hw-10

--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
-import { fetchBreeds, BASE_URL, API_KEY } from './js/cat-api';
+import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
 
-import SlimSelect from 'slim-select';
+// import SlimSelect from 'slim-select';
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
 
 const selectItemEl = document.querySelector('.breed-select');
 const showItemEl = document.querySelector('.cat-info');
 // const loaderTxt = document.querySelector('.loader');
 // const errorTxt = document.querySelector('.error');
 
-Loading.standard('Loading data, please wait...')
+// new SlimSelect({
+//   // select: '#select',
+  
+// })
+
+
+Loading.standard('Loading data, please wait...');
 
 selectItemEl.hidden = true;
 // loaderTxt.hidden = false;
 
 fetchBreeds()
   .then(data => {
-    
     Loading.remove();
 
     selectItemEl.hidden = false;
     // loaderTxt.hidden = false;
 
     data.map(({ id, name }) => {
-
       const itemList = document.createElement('option');
       selectItemEl.append(itemList);
 
       itemList.value = id;
       itemList.textContent = name;
     });
-
     // selectItemEl.hidden = false;
     // loaderTxt.hidden = true;
-
-    return data;
   })
   .catch(() => {
     Loading.remove();
@@ -45,50 +45,33 @@ fetchBreeds()
   });
 
 
-selectItemEl.addEventListener('change', fetchCatByBreed);
 
+selectItemEl.addEventListener('change', createMarkup = (data) => {
+    //   // loaderTxt.hidden = false;
+    Loading.standard('Loading data, please wait...');
+    showItemEl.innerHTML = '';
 
-function fetchCatByBreed(breedId) {
-
-  Loading.standard('Loading data, please wait...')
-  // loaderTxt.hidden = false;
-  showItemEl.innerHTML = '';
-
-  breedId = breedId.target.value;
-
-  return fetch(
-    `${BASE_URL}images/search?breed_ids=${breedId}&api_key=${API_KEY}`
-  )
-    .then(response => {
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      // console.log(response)
-      return response.json();
-    })
-
+    fetchCatByBreed(data)
     .then(data => {
-      console.log(data);
+      Loading.remove();
       for (const item of data) {
         item.breeds.map(({ name, description, temperament }) => {
           showItemEl.innerHTML = `<ul style=list-style:none>
-                <li>
-                    <img src="${item.url}" height=400px></img>
+                <li style=display:flex>
+                    <img src="${item.url}" width=400px style=margin-right:20px></img>
+                    <div>
                     <h2>${name}</h2>
                     <p>${description}</p>
                     <p><b>Temperament:</b> ${temperament}</p>
+                    </div>
                 </li>
             </ul>`;
         });
       }
-      Loading.remove();
-      // loaderTxt.hidden = true;
     })
-
     .catch(() => {
-      Loading.remove();
-      Notify.failure('Oops! Something went wrong! Try reloading the page!');
-      // loaderTxt.hidden = true;
-      // errorTxt.hidden = false;
-    });
-}
+          Notify.failure('Oops! Something went wrong! Try reloading the page!');
+      //       // loaderTxt.hidden = true;
+      //       // errorTxt.hidden = false;
+  })
+})
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,22 +1,30 @@
-
 const BASE_URL = 'https://api.thecatapi.com/v1/';
 const API_KEY =
   'live_Ltm2GBWPpfVNBkV01FC69DA1X4Xk5kGZBGw4eapAO9DTZDepX49nl7yBbmf24740';
 
-
-function fetchBreeds() {
-    return fetch(`${BASE_URL}breeds?api_key=${API_KEY}`)
-    .then(response => {
-        
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-       return response.json();
-    });
-  }
-
   
+function fetchBreeds() {
+  return fetch(`${BASE_URL}breeds?api_key=${API_KEY}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
+}
 
 
+function fetchCatByBreed(breedId) {
+  breedId = breedId.target.value;
 
-  export { fetchBreeds, BASE_URL, API_KEY };
+  return fetch(
+    `${BASE_URL}images/search?breed_ids=${breedId}&api_key=${API_KEY}`
+  ).then(response => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response.json();
+  });
+}
+
+
+export { fetchBreeds, fetchCatByBreed };
